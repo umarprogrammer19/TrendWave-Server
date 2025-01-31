bc68632aae1356473a2aa393f05a149dca697ae5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-01 01:23:55 +0500

    Full Auth Done

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDatabase from "./src/database/index.js";
+import authRouter from "./src/routes/auth.routes.js"
 
 const port = process.env.PORT;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/v1", authRouter);
 
 app.use("/", (req, res) => {
     res.send("TrendWave Server");
